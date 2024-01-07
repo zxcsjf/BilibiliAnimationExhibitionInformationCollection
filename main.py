bad1226fe25d6e7f8810575947ce01db0ab01c7c
zxcsjf <2572899924@qq.com>
2024-01-07 13:57:41 +0800

    refine address info

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ from jsonsearch import JsonSearch
 areas = [
     {"name": "上海", "code": 310100},
     {"name": "杭州", "code": 330100},
-    {"name": "苏州", "code": 320500}
-]
-typeLists = ['全部类型', '演出', '展览', '本地生活']
-pageNum = 4
+    {"name": "苏州", "code": 320500},
+    {"name": "北京", "code": 110100},
+    {"name": "丽水", "code": 331100},
+    {"name": "广州", "code": 440100}
+] # 想搜其他城市的话，去浏览器页面找到对应的code，填过来
+
+typeLists = ['展览', '演出', '本地生活', '全部类型']
+pageNum = 4 # 默认搜集4页，可以自己灵活调整
 
 resultFolder = '漫展信息/'
 
@@ -60,6 +64,7 @@ def collectEachAreaInfo(area, headers, totalResultList):
         resultList = []
         i += 1
         for page in range(1, pageNum):
+            #
             url = ("https://show.bilibili.com/api/ticket/project/listV2?version=134&page={}&pagesize=16&area={}&filter=&platform=web&p_type={}").format(
                 page, area.get("code"), urllib.parse.quote(type))
             pageContent = requests.get(url=url, headers=headers).content.decode('utf-8').split('"project_id":')
@@ -81,33 +86,34 @@ def collectEachPage(headers, pageContent, resultList):
     for activity in pageContent[1:]:
         activityName = re.compile('"project_name":"(.*?)"')
         j += 1
-        city = re.compile('"city":"(.*?)"')
+        cityRe = re.compile('"city":"(.*?)"')
         lowPrice = re.compile('"price_low":([0-9]+)')
         highPrice = re.compile('"price_high":([0-9]+)')
         startTime = re.compile('"start_time":"(.*?)"')
-        location = re.compile('"venue_name":"(.*?)"')
         url = re.compile('"url":"(.*?)"')
+        venueAddress = re.compile('"content":"(.*?)"')
 
         project_name = ''.join(activityName.findall(activity))  # 不合并是列表，合并是字符串
-        # city = ''.join((city.findall(activity)))
+        # city = ''.join((cityRe.findall(activity)))
 
         price_low = ''.join(lowPrice.findall(activity))[0:-2]
         price_high = ''.join((highPrice.findall(activity)))[0:-2]
         startTime = ''.join((startTime.findall(activity)))
-        # location = ''.join((location.findall(activity)))
         activityUrl = ''.join((url.findall(activity)))  # 活动详情页面
         id = activity.split(",")[0]  # id for find the details time range
         url = (("https://show.bilibili.com/api/ticket/project/getV2?version=134&id={}&project_id={}&requestSource=pc-new").format(
             id, id))
         details = requests.get(url=url, headers=headers).content.decode('utf-8')
         hasDancing = details.__contains__("舞")
+        # venueAddress = ''.join(venueAddress.findall(details.split('场馆地址",')[1].split("}")[0]))
 
         jsondata = JsonSearch(object=details, mode='s')
         wantToCount = JsonSearch(object=jsondata.search_first_value(key='wish_info'),
                                  mode='j').search_first_value(key='count')
 
         timeRange = jsondata.search_first_value('project_label')
-        addressDetail = jsondata.search_first_value('address_detail')
+        venue_info = JsonSearch(jsondata.search_first_value('venue_info'), mode='j').search_first_value('name')
+        addressDetail = jsondata.search_first_value('address_detail')  + ' ' + venue_info
         sale_flag = JsonSearch(jsondata.search_first_value('sale_flag'), mode='j').search_first_value('display_name')
 
         list = [startTime, project_name, addressDetail, timeRange, wantToCount,
